52c8f0d2798c1d39a8b7bcf099194c782b6846bf
srishtiagrawal <agrawalsrishti2@gmail.com>
2021-08-11 22:01:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+
+open.addEventListener('click',() => {
+    container.classList.add('show-nav');
+} )
+close.addEventListener('click',() => {
+    container.classList.remove('show-nav');
+} )
+
